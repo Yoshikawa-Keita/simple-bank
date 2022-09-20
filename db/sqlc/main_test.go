f6603dddd9e4733dfe373b133b19a76efe9fa5df
yoshikawa keita <yoshikawakei@yoshikawakeinoMacBook-Pro.local>
2022-09-20 18:55:57 +0900

    first commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	//config, err := util.LoadConfig("../..")
+	//if err != nil {
+	//	log.Fatal("cannot load config:", err)
+	//}
+	//
+	//testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	//if err != nil {
+	//	log.Fatal("cannot connect to db:", err)
+	//}
+	//
+	//testQueries = New(testDB)
+	//
+	//os.Exit(m.Run())
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
