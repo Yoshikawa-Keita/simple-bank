6f08eaa253f3a55a810655ddc047fec1c27803e8
yoshikawa keita <yoshikawakei@yoshikawakeinoMacBook-Pro.local>
2022-09-27 23:13:52 +0900

    test

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "run db migration"
+echo "I stuck in error here in the past"
 source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 echo "start the app"
